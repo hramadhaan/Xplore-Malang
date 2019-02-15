67b4f20d3f7448129b144fa37452c1e249a5e24d
Hanif Ramadhan <haniffbi@gmail.com>
2019-02-15 13:19:14 +0700

    Front End Login dan Register

---
+++ app/src/main/java/com/xploremalang/xploremalang/MainActivity.java
@@ -0,0 +1,24 @@
+package com.xploremalang.xploremalang;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.xploremalang.xploremalang.AccountActivity.LoginActivity;
+import com.xploremalang.xploremalang.AccountActivity.RegisterAccount;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pindahRegister(View view) {
+        Intent pindahRegister = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(pindahRegister);
+    }
+}
