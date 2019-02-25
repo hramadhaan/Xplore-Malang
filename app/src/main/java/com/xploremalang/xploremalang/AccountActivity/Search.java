a11eef9a0fd27788c68fc2af17262e3caf7e62ba
Dennis Cunong <deniscunong@gmail.com>
2019-02-25 17:57:15 +0700

    Fitur Search

--- app/src/main/java/com/xploremalang/xploremalang/AccountActivity/Search.java
+++ app/src/main/java/com/xploremalang/xploremalang/AccountActivity/Search.java
@@ -1,4 +1,21 @@
 package com.xploremalang.xploremalang.AccountActivity;
 
-public class Search {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.xploremalang.xploremalang.R;
+
+public class Search extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void clickExit(View v){
+        moveTaskToBack(true);
+        System.exit(1);
+    }
 }
