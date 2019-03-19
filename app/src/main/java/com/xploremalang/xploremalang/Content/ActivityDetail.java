133d8e5437bce1c73fd7ad18ee50aac267727929
denniscunong <deniscunong@gmail.com>
2019-03-19 23:55:46 +0700

    Membuat Pencarian Lokasi dengan Aplikasi Google Maps pada Kategori Pariwisata

--- app/src/main/java/com/xploremalang/xploremalang/Content/ActivityDetail.java
+++ app/src/main/java/com/xploremalang/xploremalang/Content/ActivityDetail.java
@@ -2,9 +2,12 @@ package com.xploremalang.xploremalang.Content;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,6 +38,7 @@ public class ActivityDetail extends AppCompatActivity{
     private DatabaseReference mUsers;
     Marker marker;
     private Context mContext;
+    Button btn_lokasi;
 
     private void initializeWidgets(){
         deskripsi = findViewById(R.id.tv_deskripsi);
@@ -67,6 +71,20 @@ public class ActivityDetail extends AppCompatActivity{
         String description = i.getExtras().getString("DESKRIPSI_KONTEN");
         String imageURL = i.getExtras().getString("IMAGE_KONTEN");
         String judul = i.getExtras().getString("JUDUL_KONTEN");
+
+        final String latitude = i.getExtras().getString("LATITUDE_KONTEN");
+        final String longitude = i.getExtras().getString("LONGTITUDE_KONTEN");
+
+        btn_lokasi = (Button) findViewById(R.id.btn_lokasi);
+        btn_lokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strUri = "http://maps.google.com/maps?q=loc:" + latitude + "," + longitude;
+                Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(strUri));
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                startActivity(intent);
+            }
+        });
 
         mToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
         mToolbar.setTitle(judul);
